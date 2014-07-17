1195eccb5e61c52142b8014747956c0ea45d0706
Jonathan Chang <jdkschang@gmail.com>
2014-07-17 15:44:46 -0700

    most test cases working

--- dupClass.py
+++ dupClass.py
@@ -8,10 +8,33 @@ import re
 #
 # Notes:        ---
 
-def testFile():
-    return
+filename        = 'css/testfile.txt'
+outFile         = 'css/outfile.txt'
 
-openFile = open('css/testfile.txt')
+pattern         = '\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*'
+stopPattern     = '{|}'
+outPattern      = []
+inStatement     = False
 
-for line in openFile:
-    print line
+#parse css file to extract class names
+with open(filename) as f:
+    lines = f.readlines()
+
+for line in lines:
+    match           = re.findall(pattern, line)
+    breakStartMatch = re.search(stopPattern, line)
+
+    if match and not inStatement:
+        new_line = '\n'.join(map(str, match)) + '\n'
+        outPattern.append(new_line)
+
+    # parse through until stop Pattern
+    if breakStartMatch:
+        inStatement = not inStatement
+        continue
+
+with open(outFile, 'w') as f:
+    f.seek(0)
+    f.writelines(outPattern)
+
+
