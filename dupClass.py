4f5032a4b80d23d78848a87e56733fbbfd5edc87
Jonathan Chang <jdkschang@gmail.com>
2014-07-17 10:32:21 -0700

    added css files and dupClass.py script

---
+++ dupClass.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import re
+
+# Find duplicate class names
+# 
+# Start:        July 17, 2014
+# Completed:    ---
+#
+# Notes:        ---
+
+def testFile():
+    return
+
+openFile = open('css/testfile.txt')
+
+for line in openFile:
+    print line
