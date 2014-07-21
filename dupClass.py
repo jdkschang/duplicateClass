7582c568bedf787950abfec405e3ad62e2f79545
Jonathan Chang <jdkschang@gmail.com>
2014-07-21 08:53:17 -0700

    comparisons doesnt work yet

--- dupClass.py
+++ dupClass.py
@@ -9,12 +9,13 @@ import re
 # Notes:        ---
 
 filename        = 'css/testfile.txt'
-outFile         = 'css/outfile.txt'
+outFile         = 'css/dupOut.txt'
 
 pattern         = '\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*'
 startPattern    = '{'
 stopPattern     = '}'
 outPattern      = []
+compareList     = []
 inStatement     = False
 
 #parse css file to extract class names
@@ -26,20 +27,17 @@ for line in lines:
     breakStartMatch = re.search(startPattern, line)
     breakStopMatch  = re.search(stopPattern, line)
 
-    # print 'line: %s' % line
-    # print '\tmatch: %r' % match
-    # print '\tinStatement: %s\n' % inStatement
-
+    # bracketed statement on one line
     if breakStartMatch and breakStopMatch:
-        preTest = str(line).split('{')[0]
-        sufTest = str(line).split('}')[1]
+        preTest = str(line).split('{')[0].strip() + '\n'
+        sufTest = str(line).split('}')[1].strip() + '\n'
 
         outPattern.append(preTest)
         outPattern.append(sufTest)
         continue
 
     elif match and not inStatement:
-        new_line = '\n'.join(map(str, match)) + '\n'
+        new_line = '\n'.join(map(str, match)).strip() + '\n'
         outPattern.append(new_line)
 
     # parse through until stop Pattern
@@ -48,6 +46,38 @@ for line in lines:
     elif breakStopMatch:
         inStatement = False
 
+# quick fix instead of using enumerate because of overflow in conditional
+count = 0
+for elem in outPattern:
+    print count
+    if count == 0:
+        compareList.append(elem)
+        count =+ 1
+
+    for compid,compElem in enumerate(compareList):
+        if compareList[compid] == elem:
+            print 'found dup %s' % elem
+            compareBool = True
+            break
+        else:
+            continue
+    if not compareBool:
+        compareList.append(elem)
+        count =+ 1
+    else:
+        continue
+
+# for count,elem in enumerate(outPattern):
+#     print count
+#     if count == 0:
+#         compareList.append(elem)
+#     elif compareList[count-1] == elem:
+#         continue
+#     else:
+#         compareList.append(elem)
+
+print compareList
+
 with open(outFile, 'w') as f:
     f.seek(0)
-    f.writelines(outPattern)
+    f.writelines(compareList)
